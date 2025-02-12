78a4cc336edabb488f3a95e7f054eac69b67316b
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-12 12:43:18 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ const GeneratedPage: React.FC = () => {
     <>
 		<MainLayout>
       		
+<section className="h-[92.2vh]" style={{ position: "relative", backgroundColor: "#f0f0f0" }}>
+      <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[49.16666666666667%] h-[26.623376623376625%] left-[0.9895833333333336%] top-[30.696576151121583%]" style={{ position: "absolute" }} />
+    </section>
 
     	</MainLayout>
 	</>
