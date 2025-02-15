e2dfee7506be618e3237c37c1585c2bd8e26e4bf
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2025-02-15 18:37:10 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,13 @@ import MainLayout from "@/components/layout/MainLayout";
 
 const GeneratedPage: React.FC = () => {
   return (
-    <>
-		<MainLayout>
-      		
-<section className="h-[92.2vh]" style={{ position: "relative", backgroundColor: "#f0f0f0" }}>
+    <MainLayout>
+      
+<section className="h-[106.2vh] md:h-[92.2vh]" style={{ position: "relative", backgroundColor: "#922020" }}>
       <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[49.16666666666667%] h-[26.623376623376625%] left-[0.9895833333333336%] top-[30.696576151121583%]" style={{ position: "absolute" }} />
     </section>
 
-    	</MainLayout>
-	</>
+    </MainLayout>
   );
 };
 
